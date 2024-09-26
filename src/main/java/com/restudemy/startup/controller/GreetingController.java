4098da8d10d3e6150b1b98fbda1e9fad3c59a556
Levelvini <viniciusfc29@gmail.com>
2024-09-26 11:24:02 -0300

    controller test

--- src/main/java/com/restudemy/startup/controller/GreetingController.java
+++ src/main/java/com/restudemy/startup/controller/GreetingController.java
@@ -1,9 +1,14 @@
 package com.restudemy.startup.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/greeting")
 @RestController
 public class GreetingController {
 
+    @GetMapping("/{name}")
+    private String test(@PathVariable String name){
+        return "hello " + name;
+    }
     
 }
